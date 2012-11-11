99abe6b266da8539d331cbe7a64ab7bfa331430f
Mike <Michael.Bull@live.uwe.ac.uk>
2012-11-11 19:24:33 +0000

    Modified urls.py to link to templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from django.views.generic import list_detail
+from blog.models import Post, Category
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'simpleblog.views.home', name='home'),
-    # url(r'^simpleblog/', include('simpleblog.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('blog.views',
+    (r'^$', list_detail.object_list, {'queryset': Post.objects.order_by('-created'),
+                                    'template_name': 'display_posts.html',
+                                    'template_object_name': 'post'}),
+  
+    (r'^add_post$', 'add_post'),
+    (r'^add_author$', 'add_author'),
+    (r'^add_category$', 'add_category'),
+    (r'^edit_post/(?P<id>\d+)$', 'edit_post'),
+    (r'^edit_category/(?P<id>\d+)$', 'edit_category'),
+    (r'^edit_author/(?P<id>\d+)$', 'edit_author'),
+    (r'^delete_post/(?P<id>\d+)', 'delete_post'),
+    (r'^(?P<categories>[-a-zA-Z0-9]+)/\d{4}/\d{1,2}/(?P<slug>[-a-zA-Z0-9]+)/?$', 'get_post'),
+    (r'^categories/(?P<slug>\w+)/?$', 'get_category'),
+    (r'^users/(?P<id>\d+)/(?P<username>\w+)/?$', 'get_author'),
+    (r'^delete_comment/(?P<id>\d+)', 'delete_comment'),
 )
